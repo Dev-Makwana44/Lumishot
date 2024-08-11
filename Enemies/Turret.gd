413b66efe620df816772d7d4a611feda7178cb1e
Dev Makwana <legend.destroyer.d@gmail.com>
2024-08-11 18:21:49 -0400

    Added predictor module to turrets

--- Enemies/Turret.gd
+++ Enemies/Turret.gd
@@ -17,6 +17,7 @@ signal enemy_defeated
 @onready var siren1: AnimatedSprite2D = $"Siren Container/Siren 1"
 @onready var siren_container: Node2D = $"Siren Container"
 @onready var siren_lights_container: Node2D = $"Siren Lights Container"
+@onready var predictor_lights_container: Node2D = $"Predictor Lights Container"
 
 const COLLECTABLE_ITEM: PackedScene = preload("res://Collectables/collectable.tscn")
 const BULLET_SCENE: PackedScene = preload("res://bullet.tscn")
@@ -24,10 +25,13 @@ const BULLET_SCENE: PackedScene = preload("res://bullet.tscn")
 const LIGHT_MODULE: int = 0
 const INFRARED_LIGHT_MODULE: int = 1
 const SIREN_MODULE: int = 2
+const PREDICTOR_MODULE: int = 3
 
 const LIGHT_MODULE_CHANCE: float = 0.5
 const INFRARED_LIGHT_MODULE_CHANCE: float = 0.25
 const SIREN_MODULE_CHANCE: float = 0.2
+const PREDICTOR_MODULE_CHANCE: float = 0.1
+
 
 var loot_items: Array[ItemData] = [
 	load("res://Resources/Items/CraftingItems/Bioluminescent_Bacteria.tres"),
@@ -54,7 +58,7 @@ var time_since_last_rotation: float = 0.0
 var current_rotation: float = 0.0
 var fired_this_animation: bool = false
 var alert: bool = false
-var active_modules: Array[bool] = [false, false, false]
+var active_modules: Array[bool] = [false, false, false, false]
 
 var time_since_slowed: float = SLOWNESS_DURATION
 var slowed: bool = false
@@ -75,6 +79,8 @@ func _ready():
 		active_modules[INFRARED_LIGHT_MODULE] = true
 	if rng.randf() < SIREN_MODULE_CHANCE:
 		active_modules[SIREN_MODULE] = true
+	if rng.randf() < PREDICTOR_MODULE_CHANCE:
+		active_modules[PREDICTOR_MODULE] = true
 	
 	if active_modules[LIGHT_MODULE]:
 		light.show()
@@ -84,6 +90,8 @@ func _ready():
 	if active_modules[SIREN_MODULE]:
 		siren_container.visible = true
 		siren_lights_container.visible = true
+	if active_modules[PREDICTOR_MODULE]:
+		predictor_lights_container.visible = true
 	
 	enemy_defeated.connect(room.enemy_defeated)
 	self.add_to_group("robots")
@@ -107,13 +115,14 @@ func _process(_delta):
 				var result = get_world_2d().direct_space_state.intersect_ray(query)
 				if result and result.collider is Player:
 					target_location = area.get_parent().position
-					target = area.get_parent()
+					if active_modules[PREDICTOR_MODULE]:
+						target = area.get_parent()
 		if not player_located:
 			target_location = null
-		if target_location == null:
-			turret_face.play("idle")
-		else:
+		if target_location != null or target != null:
 			turret_face.play("firing")
+		else:
+			turret_face.play("idle")
 		
 func _physics_process(delta):
 	if run:
@@ -133,15 +142,19 @@ func _physics_process(delta):
 							siren_sprite.play("alert")
 					alert = true
 		if turret_face.speed_scale != 0:
-			
 			var target_rotation: float
-			if target_location != null:
+			if active_modules[PREDICTOR_MODULE] and target != null:
+				target_rotation = predict_firing_direction(self.position + self.room.rect.position, target.position, target.velocity)
+				turret_face.rotation = lerp_angle(turret_face.rotation, target_rotation, 0.05)
+				
+			elif target_location != null:
 				target_rotation = (self.position + self.room.rect.position).angle_to_point(target_location)
 				turret_face.rotation = lerp_angle(turret_face.rotation, target_rotation, 0.05)
 			
 			elif alert:
 				target_rotation = turret_face.rotation + 0.075
 				turret_face.rotation = lerp_angle(turret_face.rotation, target_rotation, 0.05)
+				
 			else:
 				time_since_last_rotation += delta
 				if time_since_last_rotation >= TIME_BETWEEN_ROTATIONS:
@@ -150,36 +163,18 @@ func _physics_process(delta):
 				target_rotation = current_rotation
 				turret_face.rotation = lerp_angle(turret_face.rotation, target_rotation, 0.005)
 		
-func _on_face_frame_changed():
-	if turret_face.animation == "firing":
-		if turret_face.frame == 7 and not fired_this_animation:
-			shape_cast.force_shapecast_update()
-			if shape_cast.collision_result and !shape_cast.collision_result[0].collider is Player:
-				turret_face.frame = 6
-				return
-				
-			fired_this_animation = true
-			for location: Vector2 in bullet_spawn_locations.points:
-				var bullet: Bullet = BULLET_SCENE.instantiate()
-				add_sibling(bullet)
-				await bullet.is_node_ready()
-				bullet.set_bullet_type(0)
-				bullet.set_collision_mask_value(2, true)
-				bullet.set_collision_mask_value(3, true)
-				bullet.position = self.position + location.rotated(turret_face.rotation) * self.scale
-				bullet.rotation = turret_face.rotation
-				bullet.z_index = 9
-				bullet.add_to_group("enemy_bullets")
-				bullet.add_to_group("player_bullets")
-				bullet.add_collision_exception_with(self)
-			sentry_shoot.play()
-			
-		elif turret_face.frame == 0:
-			fired_this_animation = false
-	else:
-		if turret_face.frame == 0:
-			#sentry_turn.play()
-			pass
+func predict_firing_direction(starting_position: Vector2, target_position: Vector2, target_vector: Vector2) -> float:
+	var est_pos: Vector2 = target_position
+	var est_dist: float
+	var est_time: float
+	for i in range(2):
+		est_dist = starting_position.distance_to(est_pos)
+		est_time = est_dist / Bullet.SPEED
+		var old_est_pos = est_pos
+		est_pos = target_position + (target_vector * est_time)
+		if old_est_pos.distance_to(est_pos) < 50:
+			break
+	return starting_position.angle_to_point(est_pos)
 
 func damage(damage: int):
 	self.health -= damage
@@ -242,3 +237,34 @@ func _on_siren_1_frame_changed():
 	if siren1.animation == "alert":
 		for light in siren_lights_container.get_children():
 			light.enabled = (siren1.frame == 1)
+
+func _on_face_frame_changed():
+	if turret_face.animation == "firing":
+		if turret_face.frame == 7 and not fired_this_animation:
+			shape_cast.force_shapecast_update()
+			if shape_cast.collision_result and !shape_cast.collision_result[0].collider is Player:
+				turret_face.frame = 6
+				return
+				
+			fired_this_animation = true
+			for location: Vector2 in bullet_spawn_locations.points:
+				var bullet: Bullet = BULLET_SCENE.instantiate()
+				add_sibling(bullet)
+				await bullet.is_node_ready()
+				bullet.set_bullet_type(0)
+				bullet.set_collision_mask_value(2, true)
+				bullet.set_collision_mask_value(3, true)
+				bullet.position = self.position + location.rotated(turret_face.rotation) * self.scale
+				bullet.rotation = turret_face.rotation
+				bullet.z_index = 9
+				bullet.add_to_group("enemy_bullets")
+				bullet.add_to_group("player_bullets")
+				bullet.add_collision_exception_with(self)
+			sentry_shoot.play()
+			
+		elif turret_face.frame == 0:
+			fired_this_animation = false
+	else:
+		if turret_face.frame == 0:
+			#sentry_turn.play()
+			pass
